c59bfe3630c7ea93918101a878e47055986d6f8c
levi <levi.liester@gmail.com>
2017-01-14 16:15:52 -0600

    Testing update

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-      return "Hello World!";
+      return "Hello World after updates!";
     }
 
     public static void main(String[] args) {
